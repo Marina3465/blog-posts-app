6a1714a4f3e8594a437e40fd480d67529ac55d44
Marina Kozhukhar <marina.kozhukhar@jetruby.com>
2026-09-23 18:20:45 +0300

    Add repeate background

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,7 +2,10 @@ import { Input } from "@/shared/ui/Input";
 
 export const Login = () => {
   return (
-    <div className="w-full h-dvh flex justify-center items-center">
+    <div
+      className="w-full h-dvh flex justify-center items-center bg-repeat bg-size-[450px_490px] animate-bg-drift"
+      style={{ backgroundImage: "url('/icons/background-tile.jpeg')" }}
+    >
       <div className="bg-white p-10 border border-gray-200 grid gap-6 rounded-2xl">
         <Input type="email" />
         <Input type="password" />
